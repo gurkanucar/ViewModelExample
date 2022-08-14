c35548c4f52a32ba234a182ea021528b25df8f02
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-08-14 18:43:21 +0300

    view model created

---
+++ app/src/main/java/com/gusoft/viewmodelexample/DataViewModel.kt
@@ -0,0 +1,18 @@
+package com.gusoft.viewmodelexample
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DataViewModel : ViewModel() {
+    var number: Int = 0
+    val currentNumber: MutableLiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+
+    val currentBoolean: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+
+
+
+}
